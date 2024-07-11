7e05948da6009fd263948674f7cc7cd09146888e
seunghwan <lovelyunsh@naver.com>
2024-07-11 15:33:04 +0900

    4.반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정합니다.

--- src/App.java
+++ src/App.java
@@ -2,34 +2,40 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
-        sc.nextLine();
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char operator = sc.nextLine().charAt(0);
-        int result = 0;
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 == 0) {
-                    System.out.println("분모는 0이 될 수 없음.");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
+            sc.nextLine();
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.nextLine().charAt(0);
+            int result = 0;
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
                     break;
-                }
-                result = num1 / num2;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("분모는 0이 될 수 없음.");
+                        break;
+                    }
+                    result = num1 / num2;
+                    break;
+            }
+            System.out.println("결과: " + result);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            if(sc.nextLine().equals("exit"))
                 break;
         }
-        System.out.println("결과: " + result);
     }
 }
