b78ebe74d0bbf274a167ab49df09fe37b10ee7f7
seunghwan <lovelyunsh@naver.com>
2024-07-11 15:27:42 +0900

    2.Scanner를 사용하여 사칙연산 기호를 전달 받을 수 있습니다.

--- src/App.java
+++ src/App.java
@@ -7,6 +7,9 @@ public class App {
         System.out.print("첫 번째 숫자를 입력하세요: ");
         int num1 = sc.nextInt();
         System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 =  sc.nextInt();
+        int num2 = sc.nextInt(); sc.nextLine();
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.nextLine().charAt(0);
+
     }
 }
